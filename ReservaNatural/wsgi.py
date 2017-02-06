543ca672b247705ebe6683cca98b0cf931399b94
yedgar <yedgar@gmail.com>
2017-02-06 11:41:05 -0500

    Inicial

---
+++ ReservaNatural/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ReservaNatural project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ReservaNatural.settings")
+
+application = get_wsgi_application()
