eb5ab6803f76ae09062c5e3df73788911adf9732
yedgar <yedgar@gmail.com>
2017-02-07 13:22:10 -0500

    Inicial

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # from .models import Imagen
 # Register your models here.
+from polls.models import Categoria, Especie, Comentario, Usuario
 
-# admin.site.register(Imagen)
+admin.site.register(Categoria)
+admin.site.register(Especie)
+admin.site.register(Comentario)
+admin.site.register(Usuario)
--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Especie(models.Model):
+    nombre = models.CharField(max_length=100)
+    nombre_cientifico = models.CharField(max_length=100)
+    clasificacion_taxonomica = models.CharField(max_length=200)
+    descripcion_corta = models.CharField(max_length=200)
+    descripcion_larga = models.CharField(max_length=500)
+    fotoFile = models.ImageField(upload_to='images', null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+
+class Comentario(models.Model):
+    descripcion = models.CharField(max_length=500)
+    correo_electronico = models.CharField(max_length=100)
+    especie = models.ForeignKey(Especie, on_delete=models.CASCADE, null=True)
+
 class Pais(models.Model):
     nombre = models.CharField(max_length=100)
 
